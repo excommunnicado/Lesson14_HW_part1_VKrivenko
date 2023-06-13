3585e3d33f5302d9ca6b0b9398e9df362867cd61
Krivenko Vassiliy <VKrivenko@beeline.kz>
2023-06-13 20:01:36 +0600

    Домашнее задание 14 (часть 1)

---
+++ main.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+        firstName: '',
+        lastName: '',
+        middleName: '',
+        country: '',
+        cityExcursion: false,
+        suburbExcursion: false,
+        withoutGuideExcursion: false,
+        baggage: '',
+        comments: ''
+    }
+});
